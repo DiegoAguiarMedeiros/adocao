2d6e898476f5079698ef54bc59794b2018a9c87c
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2022-10-28 14:36:43 -0300

    fix rota de login

--- api/auth/rest-auth.js
+++ api/auth/rest-auth.js
@@ -1,6 +1,6 @@
 import HttpClient from '../http-client';
 
-const postAuth = (data) => HttpClient.post('auth', data);
+const postAuth = (data) => HttpClient.post('usersAuth', data);
 
 export default {
   postAuth,
